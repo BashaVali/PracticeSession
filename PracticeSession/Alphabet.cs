b29d7db5bce1b5d953afd54d1d492916f4059530
BashaVali <bashavali222@gmail.com>
2022-11-11 11:32:33 +0530

    Practice Session

---
+++ PracticeSession/Alphabet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Alphabet
+    {
+        public void Aplhabets()
+        {
+            char c;
+            Console.WriteLine("\n");
+            for (c = 'A'; c <= 'Z'; c++)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
+
---
+++ PracticeSession/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine(" \n \n Practice Session");
+                Console.WriteLine("Press The Below Options");
+                Console.WriteLine(" 1.Alphabet \n  2.Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        Alphabet alphabets = new Alphabet();
+                        alphabets.Aplhabets();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
+
